594cfcac5505102ea79f65188f48aa78746b95af
Bruce Banfield <brucebanfield96@hotmail.com>
2023-01-21 11:38:48 -0400

    Finished tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import StarWarsCharacter from './components/Character';
+import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +12,34 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starwarsCharacter, setStarWarsCharacter] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setStarWarsCharacter(res.data);
+    })
+    .catch(err => console.error(err))
+  }, []);
+
+  const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 3px;
+  border: 2px solid green;
+  `
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledDiv>
+        {starwarsCharacter.map((characterData) => {
+          return <StarWarsCharacter data = {characterData} />
+        })}
+      </StyledDiv>
     </div>
+    
+
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+border-radius: 3px;
+border: 2px solid green;
+`;
+
+const StarWarsCharacter = props => {
+    const {data} = props
+    return (
+        <StyledDiv>
+            <h1> Details for {data.name}</h1>
+            <p> Character's BirthYear: {data.birth_year}</p>
+            <p> Character's Height: {data.height}</p>
+            <p> Character's Gender: {data.gender}</p>
+        </StyledDiv>
+    )
+}
+
+export default StarWarsCharacter;
